d2d14f126f15b7654eeb24502fe2a1aadc0ed3d3
Hilary02 <hilary02e@gmail.com>
2021-07-07 13:10:29 +0000

    gitconfig

---
+++ setup_zsh.sh
@@ -0,0 +1,8 @@
+# zsh is already installed
+# install zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
