d494b7dee828a1a005d6d56e43ea791d03a1e12b
Hilary02 <hilary02e@gmail.com>
2021-07-08 00:12:01 +0000

    add link.sh

---
+++ link.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# nounset
+set -u
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY=".config"
+
+echo "link home directory dotfiles"
+cd ${DOT_DIRECTORY}
+for f in .??*
+do
+    # ignore files
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".config" ] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+
+echo "link .config directory dotfiles"
+cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
+for file in `\find . -maxdepth 8 -type f`; do
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
+done
+
+echo "linked dotfiles complete!"
