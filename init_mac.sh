156bdb0856ba74a03008de03627ea6fd9d4909ba
hilary02 <hilary02e@gmail.com>
2024-02-18 15:59:05 +0900

    Add permission update and disable music auto-start

--- init_mac.sh
+++ init_mac.sh
@@ -4,6 +4,9 @@
 ./link.sh
 ./setup_zsh.sh
 
+# update permission
+chmod 744 ./backup_mac.sh
+
 # Install Homebrew
 echo "installing Homebrew ..."
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -26,6 +29,11 @@ brew cleanup
 # vscodeがインストールされた後にvscodeの設定コピーを実行
 ./vscode/vscode_mac.sh
 
+# musicの自動起動の無効化
+# https://randomsense.net/play-arbitrarily-applemusic/
+launchctl disable gui/"$(id -u)"/com.apple.rcd
+launchctl kill SIGTERM gui/"$(id -u)"/com.apple.rcd
+
 # defaults系の変更
 # see: https://do-zan.com/mac-dock-terminalcommand-settings/
 defaults write com.apple.dock mineffect -string "scale"
@@ -35,5 +43,11 @@ defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0.02
 defaults write com.apple.dock autohide-time-modifier -float 0.1
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+# スクショの影を消す
+defaults write com.apple.screencapture disable-shadow -boolean true
+defaults write -g NSWindowResizeTime 0.001
+defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 # reboot Dock
 killall Dock
